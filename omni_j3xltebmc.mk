df1fa0106f57f22fd0d37a928c862163b760d624
NotNoelChannel <notnoel12345678@gmail.com>
2025-01-22 17:12:00 +0100

    j3xltebmc: initial

--- omni_j3xltebmc.mk
+++ omni_j3xltebmc.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := on5ltetmo
+PRODUCT_RELEASE_NAME := j3xltebmc
 
 $(call inherit-product, build/target/product/embedded.mk)
 
@@ -7,7 +7,7 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := on5ltetmo
-PRODUCT_NAME := omni_on5ltetmo
+PRODUCT_DEVICE := j3xltebmc
+PRODUCT_NAME := omni_j3xltebmc
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
